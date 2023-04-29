9228297fcd2557cd7d53e780d5ff7b602aca0f5d
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-04-30 01:39:36 +0300

    Added Prettier and Function

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends cdk.Stack {
@@ -10,12 +12,18 @@ export class AwsMicroservicesStack extends cdk.Stack {
     const productTable = new Table(this, 'product', {
       partitionKey: {
         name: 'id',
-        type: AttributeType.STRING
+        type: AttributeType.STRING,
       },
       tableName: 'product',
       // it will destroy table when destory cdk is executed
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      billingMode: BillingMode.PAY_PER_REQUEST
-    })
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    const fn = new Function(this, 'MyFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromAsset(join(__dirname, 'lambda-handler')),
+    });
   }
 }
