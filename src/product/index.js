cc7b7d6f3a777ffd55f8bf30ceb196e4668353c2
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-04 11:10:28 +0300

    Code Format

--- src/product/index.js
+++ src/product/index.js
@@ -1,46 +1,42 @@
 import { GetItemCommand } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
-import { ddbClient } from './ddbClient'
+import { ddbClient } from './ddbClient';
 
 // lambda function
 exports.handler = async function (event) {
-    console.log("request:", JSON.stringify(event, undefined, 2));
+    console.log('request:', JSON.stringify(event, undefined, 2));
 
     // TODO: Switch case on event.httpmethod to perform CRUD operations
     switch (event.httpmethod) {
-        case "GET":
+        case 'GET':
             if (event.pathParameters != null)
                 body = await getProduct(event.pathParameters.id); // GET product/1
-            else
-                body = await getAllProducts(); // GET all products
+            else body = await getAllProducts(); // GET all products
     }
-
 
     return {
         statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello from Product ! You've hit ${event.path}\n`
+        headers: { 'Content-Type': 'text/plain' },
+        body: `Hello from Product ! You've hit ${event.path}\n`,
     };
-}
-
+};
 
 const getProduct = async (productId) => {
-    console.log("getProduct:", productId);
+    console.log('getProduct:', productId);
 
     try {
         const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
             // partication Id key
-            Key: marshall({ id: productId })
+            Key: marshall({ id: productId }),
         };
 
         const { Item } = await ddbClient.send(new GetItemCommand(params));
 
         console.log(Item);
-        return (Item) ? unmarshall(Item) : {};
+        return Item ? unmarshall(Item) : {};
     } catch (e) {
         console.error(e);
         throw e;
     }
-
 };
