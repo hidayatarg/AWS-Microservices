3aaec4ed003d92190c3a46b4b42ca7092b2881e8
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-08 14:54:47 +0300

    Added SwnApiGateway - a Central ApiGatway Class

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,15 +1,8 @@
 import { SwnDatabase } from './database';
 import * as cdk from 'aws-cdk-lib';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import {
-  NodejsFunction,
-  NodejsFunctionProps,
-} from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-import { join } from 'path';
 import { SwnMicroservices } from './microservices';
+import { SwnApiGateway } from './apigatway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends cdk.Stack {
@@ -21,22 +14,9 @@ export class AwsMicroservicesStack extends cdk.Stack {
     const microservice = new SwnMicroservices(this, 'Microservices', {
       productTable: database.productTable,
     });
-    // Product microservice API Gateway
-    // root name = product
-    const apigw = new LambdaRestApi(this, 'productApi', {
-      restApiName: 'Product Service',
-      handler: microservice.productMicroservices,
-      // create routes => not directing all a proxy
-      proxy: false,
+
+    const apigateway = new SwnApiGateway(this, 'Apigateway', {
+      productMicroservices: microservice.productMicroservices,
     });
-
-    const product = apigw.root.addResource('product');
-    product.addMethod('GET'); // GET /product
-    product.addMethod('POST'); // POST /product
-
-    const singleProduct = product.addResource('{id}'); // product/{id}
-    singleProduct.addMethod('GET'); // GET /product/{id}
-    singleProduct.addMethod('PUT'); // PUT /product/{id}
-    singleProduct.addMethod('DELETE'); // DELETE /product/{id}
   }
 }
