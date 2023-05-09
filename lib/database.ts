f43e0208c1d0c2a0ef6d6e60411ef83baf4415e1
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-10 00:13:11 +0300

    Created DynamoDb For Basket

--- lib/database.ts
+++ lib/database.ts
@@ -9,14 +9,17 @@ import {
 import { Construct } from 'constructs';
 
 export class SwnDatabase extends Construct {
-  // expose product table
+  // expose table
   public readonly productTable: ITable;
+  public readonly basketTable: ITable;
 
   //constructor requires two parameters
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // product dynamodb creation
+    // product: PK: id --name -description -imageFile -price -category
+    // these are fields we can pass through post request (client)
     const productTable = new Table(this, 'product', {
       partitionKey: {
         name: 'id',
@@ -30,5 +33,23 @@ export class SwnDatabase extends Construct {
 
     // product expose
     this.productTable = productTable;
+
+    // basket dynamodb creation
+    // basket: PK: username -basketItemList (SET-MAP object) (Nosql cannbe in same table in json)
+    // item1 -> {quantity - color - price - productId - productName}
+    // item2 -> {quantity - color - price - productId - productName}
+    const basketTable = new Table(this, 'basket', {
+      partitionKey: {
+        name: 'userName',
+        type: AttributeType.STRING,
+      },
+      tableName: 'basket',
+      // it will destroy table when destory cdk is executed
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    // basket expose
+    this.basketTable = basketTable;
   }
 }
