267febfe0e85f4358c4512878894bc7e59d4eb7b
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-11 01:02:07 +0300

    Added Basket Lambda Microservices Functions -1

--- src/basket/index.js
+++ src/basket/index.js
@@ -1,8 +1,87 @@
+import { DeleteItemCommand, GetItemCommand, PutItemCommand, QueryCommand, ScanCommand, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { ddbClient } from './ddbClient';
+import { v4 as uuidv4 } from 'uuid';
+
+// lambda function
 exports.handler = async function (event) {
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello from Basket ! You've hit ${event.path}\n`
-    };
+    console.log('request:', JSON.stringify(event, undefined, 2));
+    // TODO: Switch case on event.httpMethod to perform add/remove basket
+    // and checkout basket operations with using ddbClient object
+
+    // GET /basket
+    // POST /basket
+    // GET /basket/{userName}
+    // DELETE /basket/{userName}
+    // POST /basket/checkout
+
+
+    let body;
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                if (event.pathParameters != null) {
+                    body = await getBasket(event.pathParameters.userName); // GET /basket/{userName}
+                } else {
+                    body = await getAllBaskets(); // GET /basket
+                }
+                break;
+            case 'POST':
+                if (event.path == "/basket/checkout") {
+                    body = await checkoutBasket(event); // POST /basket/checkout
+                } else {
+                    body = await createBasket(event); // POST /basket
+                }
+                break;
+            case "DELETE":
+                body = await deleteBasket(event.pathParameters.userName); // DELETE /basket
+                break;
+            default:
+                throw new Error(`Unsupported route: "${event.httpmethod}"`);
+        }
+        console.log("body: ", body);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Successfully finished operation: "${event.httpMethod}"`,
+                body: body
+            })
+        };
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Failed to perform operation.",
+                errorMsg: e.message,
+                errorStack: e.stack,
+            })
+        };
+    }
+};
+
+const getBasket = async (userName) => {
+    console.log('getBasket');
+    // function getBasket
+};
+
+const getAllBaskets = async () => {
+    console.log('getAllBaskets');
+    // function getAllBaskets
+};
+
+const createBasket = async (event) => {
+    console.log('createBasket');
+    // function createBasket
+};
+
+const deleteBasket = async (userName) => {
+    console.log('deleteBasket');
+    // function deleteBasket
+};
+
+const checkoutBasket = async (event) => {
+    console.log('checkoutBasket');
+    // function checkoutBasket
 };
